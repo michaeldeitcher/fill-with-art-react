4c2ff86434abe939b0a57d12077cf2c6bdfa1630
Michael Deitcher <michaeldeitcher@gmail.com>
2020-02-28 22:34:42 -0700

    trying to get image conversion working on mobile safari

--- src/components/ImageCanvas.js
+++ src/components/ImageCanvas.js
@@ -9,42 +9,42 @@ export default function ImageCanvas(props) {
 
     const adjustCanvasSize = () => {
         const ratio = 85/110;
-        const margin = 20;
+        const margin = 100;
         const maxWidth = window.innerWidth - margin;
         const maxHeight = window.innerHeight - margin;
-        if( maxWidth < width ) {
+        if( maxWidth < maxHeight ) {
             setWidth(maxWidth);
-            setHeight(ratio/maxWidth);
-        }
-        if( maxHeight < height ) {
+            setHeight(Math.floor(maxWidth/ratio));
+        } else  {
             setHeight(maxHeight);
-            setWidth(ratio*maxHeight);
+            setWidth(Math.floor(maxHeight*ratio));
         }
     }
 
     useEffect( () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-        const img = imgRef.current;
-
+        let img = new Image();        
         img.onload = () => {
             // draw image center filling canvas at the same aspect ratio as image
+            console.log(img.height + ' ' + img.width);
             const imgRatio = img.width / img.height;
-            if( img.height < height ) {
-                let drawWidth = height * imgRatio;
-                ctx.drawImage(img, -(drawWidth-width)/2, 0, drawWidth, height);    
+            if(img.height > img.width){
+                let drawHeight = width / imgRatio;
+                ctx.drawImage(img, 0, -(drawHeight - height)/2, width, drawHeight); 
             } else {
-                ctx.drawImage(img, 0, -(img.height - height)/2, width, img.height);    
+                let drawWidth = height * imgRatio;
+                ctx.drawImage(img, -(drawWidth - width)/2, 0, drawWidth, height);                 
             }
             canvas.toBlob( blob => props.onImageBlob(blob));
         }
+        img.src = props.image;
         adjustCanvasSize();    
-    }, []);
+    }, [props.image]);
 
     return (
       <div>
         <canvas ref={canvasRef} width={width} height={height} />
-        <img ref={imgRef} src={props.image} className="hidden" width={width} alt=''/>
       </div>
     )
 }
